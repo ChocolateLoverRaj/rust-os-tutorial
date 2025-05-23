21b1e75298cdbee640be252d268e2c5d2d1e3af8
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-05-22 22:51:08 -0700

    Part 7

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,11 +3,13 @@
 
 extern crate alloc;
 
+use cpu_local_data::init_cpu;
 use hlt_loop::hlt_loop;
 use limine_requests::{BASE_REVISION, HHDM_REQUEST, MEMORY_MAP_REQUEST, MP_REQUEST};
 use memory::MEMORY;
 use x86_64::registers::control::Cr3;
 
+pub mod cpu_local_data;
 pub mod cut_range;
 pub mod hhdm_offset;
 pub mod hlt_loop;
@@ -35,9 +37,22 @@ unsafe extern "C" fn entry_point_from_limine() -> ! {
     let mp_response = MP_REQUEST.get_response().unwrap();
     let cpu_count = mp_response.cpus().len();
     log::info!("CPU Count: {}", cpu_count);
+    cpu_local_data::init(mp_response);
+    unsafe {
+        init_cpu(
+            mp_response,
+            mp_response
+                .cpus()
+                .iter()
+                .find(|cpu| cpu.lapic_id == mp_response.bsp_lapic_id())
+                .unwrap(),
+        );
+    }
     for cpu in mp_response.cpus() {
         cpu.goto_address.write(entry_point_from_limine_mp);
     }
+
+    log::info!("Hello from BSP");
 
     todo!()
 }
@@ -46,6 +61,8 @@ unsafe extern "C" fn entry_point_from_limine_mp(cpu: &limine::mp::Cpu) -> ! {
     let memory = MEMORY.try_get().unwrap();
     // Safety: This function is only executed after memory is initialized
     unsafe { Cr3::write(memory.new_kernel_cr3, memory.new_kernel_cr3_flags) };
+    // Safety: We're inputting the correct CPU
+    unsafe { init_cpu(MP_REQUEST.get_response().unwrap(), cpu) };
 
     let cpu_id = cpu.id;
     log::info!("Hello from CPU {}", cpu_id);
