bde18563f02addc46af37d439a6b4f5b3fabfdb5
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-05-26 11:00:45 -0700

    part 8 - add kernel data segment to GDT

--- kernel/src/gdt.rs
+++ kernel/src/gdt.rs
@@ -1,6 +1,6 @@
 use x86_64::{
     instructions::tables::load_tss,
-    registers::segmentation::{CS, DS, ES, SS, Segment},
+    registers::segmentation::{CS, SS, Segment},
     structures::{
         gdt::{Descriptor, GlobalDescriptorTable, SegmentSelector},
         tss::TaskStateSegment,
@@ -12,6 +12,7 @@ use crate::cpu_local_data::get_local;
 pub struct Gdt {
     gdt: GlobalDescriptorTable,
     kernel_code_selector: SegmentSelector,
+    kernel_data_selector: SegmentSelector,
     tss_selector: SegmentSelector,
 }
 
@@ -29,10 +30,12 @@ pub unsafe fn init() {
             .try_init_once(|| {
                 let mut gdt = GlobalDescriptorTable::new();
                 let kernel_code_selector = gdt.append(Descriptor::kernel_code_segment());
+                let kernel_data_selector = gdt.append(Descriptor::kernel_data_segment());
                 let tss_selector = gdt.append(Descriptor::tss_segment(tss));
                 Gdt {
                     gdt,
                     kernel_code_selector,
+                    kernel_data_selector,
                     tss_selector,
                 }
             })
@@ -41,8 +44,6 @@ pub unsafe fn init() {
     };
     gdt.gdt.load();
     unsafe { CS::set_reg(gdt.kernel_code_selector) };
-    unsafe { SS::set_reg(SegmentSelector::NULL) };
-    unsafe { DS::set_reg(SegmentSelector::NULL) };
-    unsafe { ES::set_reg(SegmentSelector::NULL) };
+    unsafe { SS::set_reg(gdt.kernel_data_selector) };
     unsafe { load_tss(gdt.tss_selector) };
 }
