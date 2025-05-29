b060dab4179e46b9a8c065b388723074ca55878c
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-05-29 10:14:51 -0700

    CPU Local Data
    Originally, a boxed slice was used. This was changed to a BTreeMap.
    
    Improve CPU local data
    
    Key improvement: Boxing the cpu local data

---
+++ kernel/src/cpu_local_data.rs
@@ -0,0 +1,49 @@
+use alloc::{boxed::Box, collections::btree_map::BTreeMap};
+use conquer_once::noblock::OnceCell;
+use limine::{mp::Cpu, response::MpResponse};
+use x86_64::{VirtAddr, registers::model_specific::GsBase};
+
+pub struct CpuLocalData {
+    pub cpu: &'static Cpu,
+}
+
+static CPU_LOCAL_DATA: OnceCell<BTreeMap<u32, Box<CpuLocalData>>> = OnceCell::uninit();
+
+pub fn init(mp_response: &'static MpResponse) {
+    CPU_LOCAL_DATA
+        .try_init_once(|| {
+            mp_response
+                .cpus()
+                .into_iter()
+                .map(|cpu| (cpu.lapic_id, Box::new(CpuLocalData { cpu })))
+                .collect()
+        })
+        .unwrap();
+}
+
+/// This function makes sure that we are writing a valid pointer to CPU local data to GsBase
+fn write_gs_base(ptr: &'static CpuLocalData) {
+    GsBase::write(VirtAddr::from_ptr(ptr));
+}
+
+/// # Safety
+/// The Local APIC id must match the actual CPU that this function is called on
+pub unsafe fn init_cpu(local_apic_id: u32) {
+    write_gs_base(
+        CPU_LOCAL_DATA
+            .try_get()
+            .unwrap()
+            .get(&local_apic_id)
+            .unwrap(),
+    );
+}
+
+pub fn get_local() -> &'static CpuLocalData {
+    try_get_local().unwrap()
+}
+
+pub fn try_get_local() -> Option<&'static CpuLocalData> {
+    let ptr = GsBase::read().as_ptr::<CpuLocalData>();
+    // Safety: we only wrote to GsBase using `write_gs_base`, which ensures that the pointer is `&'static CpuLocalData`
+    unsafe { ptr.as_ref() }
+}
