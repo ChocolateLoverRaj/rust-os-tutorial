b738082841304061458a95043b81d4c3dd4ada4f
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-06-04 12:31:19 -0700

    Remove `is_even` test function

--- user_mode_program/src/main.rs
+++ user_mode_program/src/main.rs
@@ -10,10 +10,6 @@ fn rust_panic(_info: &core::panic::PanicInfo) -> ! {
 
 #[unsafe(no_mangle)]
 unsafe extern "C" fn entry_point() -> ! {
-    fn is_even(n: u64) -> bool {
-        n % 2 == 0
-    }
-    is_even(3);
     loop {
         core::hint::spin_loop();
     }
