8f5c880b0cf06e7fdf4f7279590a57f244e89938
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-07-31 22:02:20 -0700

    zerocopy map_module syscall

--- kernel/src/syscall_handlers/map_module.rs
+++ kernel/src/syscall_handlers/map_module.rs
@@ -1,18 +1,20 @@
-use alloc::{collections::btree_set::BTreeSet, format};
+use alloc::format;
 use common::{HIGHER_HALF_START, SliceData, SyscallMapModule, SyscallMapModuleError};
 use nodit::interval::ee;
 use x86_64::{
-    VirtAddr,
-    structures::paging::{Mapper, Page, PageSize, PageTableFlags, Size4KiB, mapper::MapToError},
+    PhysAddr, VirtAddr,
+    structures::paging::{
+        Mapper, Page, PageSize, PageTableFlags, PhysFrame, Size4KiB, mapper::MapToError,
+    },
 };
 
 use crate::{
     cpu_local_data::get_local,
     get_page_table::get_page_table,
+    hhdm_offset::HhdmOffset,
     limine_requests::MODULE_REQUEST,
-    memory::{MEMORY, MemoryType},
+    memory::MEMORY,
     task::{THREADS, UserVirtMem},
-    translate_addr::GetFrameSlice,
 };
 
 use super::GenericSyscallHandler;
@@ -31,9 +33,10 @@ impl GenericSyscallHandler for SyscallMapModuleHandler {
                 .find(|file| file.path().to_str() == Ok(&format!("/extra_module_{index}")))
                 .ok_or(SyscallMapModuleError::NotPresent)?;
             let len = module.size();
-            let module_ptr = module.addr();
-            // Safety: module slice is valid and only immutably accessed
-            let module_slice = unsafe { core::slice::from_raw_parts(module_ptr, len as usize) };
+            let first_frame = PhysFrame::<Size4KiB>::from_start_address(PhysAddr::new(
+                module.addr() as u64 - u64::from(HhdmOffset::get_from_response()),
+            ))
+            .unwrap();
             let n_pages = len.div_ceil(Size4KiB::SIZE);
             let threads = THREADS.read();
             let local = get_local();
@@ -65,23 +68,11 @@ impl GenericSyscallHandler for SyscallMapModuleHandler {
             let memory = MEMORY.get().unwrap();
             let mut physical_memory = memory.physical_memory.lock();
             for i in 0..n_pages {
-                let frame = physical_memory
-                    .allocate_frame_with_type(MemoryType::UsedByUserMode(BTreeSet::from([
-                        current_process.id,
-                    ])))
-                    .ok_or(SyscallMapModuleError::OutOfPhysicalMemory)?;
-                // Safety: we have an exclusive reference
-                let frame_slice = unsafe { frame.get_slice_mut() };
-                let copy_start = (i * Size4KiB::SIZE) as usize;
-                let bytes_to_copy = (module_slice.len() - copy_start).min(Size4KiB::SIZE as usize);
-                frame_slice[..bytes_to_copy]
-                    .copy_from_slice(&module_slice[copy_start..copy_start + bytes_to_copy]);
-                // In the last frame, zero unused bytes
-                frame_slice[bytes_to_copy..].fill(0);
                 let flags = PageTableFlags::PRESENT
                     | PageTableFlags::USER_ACCESSIBLE
                     | PageTableFlags::NO_EXECUTE;
                 let page = start_page + i;
+                let frame = first_frame + i;
                 let frame_allocator =
                     &mut physical_memory.get_user_mode_program_frame_allocator(current_process.id);
                 unsafe { mapper.map_to(page, frame, flags, frame_allocator) }
@@ -92,6 +83,16 @@ impl GenericSyscallHandler for SyscallMapModuleHandler {
                         e => unreachable!("{:#?}", e),
                     })?
                     .flush();
+            }
+            // Zero unused bytes of last frame
+            // If this module gets mapped multiple times, we don't need to zero the bytes the 2nd+ times
+            // But to keep things simple we can just zero every time
+            let bytes_to_copy = (n_pages * Size4KiB::SIZE - len) as usize;
+            unsafe {
+                module
+                    .addr()
+                    .add(len as usize)
+                    .write_bytes(Default::default(), bytes_to_copy);
             }
             Ok(SliceData::new(*range.start(), len))
         })();
