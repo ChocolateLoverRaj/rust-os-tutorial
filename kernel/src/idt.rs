b8683c208e6fa6432f9bf36df2d090ece3d7895a
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-08-31 11:36:59 -0700

    Part 12
    Handle page faults
    
    Add stack switching exception handlers to part 9
    
    Use boxed stacks for IST
    
    Going to split this into multiple parts. THis part just handles simple page faults.

--- kernel/src/idt.rs
+++ kernel/src/idt.rs
@@ -1,4 +1,7 @@
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::{
+    registers::control::Cr2,
+    structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode},
+};
 
 use crate::cpu_local_data::get_local;
 
@@ -6,10 +9,21 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     log::info!("Breakpoint! Stack frame: {stack_frame:#?}");
 }
 
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    let accessed_address = Cr2::read().unwrap();
+    panic!(
+        "Page fault! Stack frame: {stack_frame:#?}. Error code: {error_code:#?}. Accessed address: {accessed_address:?}."
+    )
+}
+
 pub fn init() {
     let idt = get_local().idt.call_once(|| {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt
     });
     idt.load();
