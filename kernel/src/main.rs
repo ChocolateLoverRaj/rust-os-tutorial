c340e51299c7ea3f166a42631508a6edf884552d
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-09-02 11:32:44 -0700

    Part 8
    Global allocator
    
    Just global allocator, no page table modifications yet
    
    Bring some changes from `next` branch
    
    Add a convenient hhdm_offset() fn
    
    Create phys to virt translation helper trait

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,19 +1,25 @@
 #![no_std]
 #![no_main]
+extern crate alloc;
 
 use frame_buffer_embedded_graphics::*;
 use frame_buffer_info::*;
+use hhdm_offset::*;
 use hlt_loop::*;
 use limine_requests::*;
 use rgb_pixel_info::*;
+use translate_addr::*;
 
 mod frame_buffer_embedded_graphics;
 mod frame_buffer_info;
+mod hhdm_offset;
 mod hlt_loop;
 mod limine_requests;
 mod logger;
+mod memory;
 mod panic_handler;
 mod rgb_pixel_info;
+mod translate_addr;
 
 #[unsafe(no_mangle)]
 unsafe extern "C" fn entry_point_from_limine() -> ! {
@@ -24,6 +30,10 @@ unsafe extern "C" fn entry_point_from_limine() -> ! {
     let frame_buffer_response = FRAME_BUFFER_REQUEST.get_response().unwrap();
     logger::init(frame_buffer_response).unwrap();
     log::info!("Hello from BSP");
+
+    let memory_map = MEMORY_MAP_REQUEST.get_response().unwrap();
+    // Safety: we are initializing this for the first time
+    unsafe { memory::init(memory_map) };
 
     let mp_response = MP_REQUEST.get_response().unwrap();
     for cpu in mp_response.cpus() {
