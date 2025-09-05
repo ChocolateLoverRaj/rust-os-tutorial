edeedf1679476d7164540acc90fbaa2a6ff5753b
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-09-05 13:27:11 -0700

    Part 15
    Double fault handler

--- kernel/src/idt/mod.rs
+++ kernel/src/idt/mod.rs
@@ -9,6 +9,13 @@ extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     log::info!("Breakpoint! Stack frame: {stack_frame:#?}");
 }
 
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: u64,
+) -> ! {
+    panic!("Double Fault! Stack frame: {stack_frame:#?}. Error code: {error_code}.")
+}
+
 pub fn init() {
     let idt = get_local().idt.call_once(|| {
         let mut idt = InterruptDescriptorTable::new();
@@ -16,6 +23,11 @@ pub fn init() {
         unsafe {
             idt.page_fault
                 .set_handler_fn(page_fault_handler)
+                .set_stack_index(u8::from(IstStackIndexes::Exception).into())
+        };
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(double_fault_handler)
                 .set_stack_index(u8::from(IstStackIndexes::Exception).into())
         };
         idt
