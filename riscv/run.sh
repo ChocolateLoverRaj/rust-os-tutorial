95086337730877b3fe81f2f4c866cca1638e2fd4
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2026-02-13 09:24:56 -0800

    Move riscv to folder

--- riscv/run.sh
+++ riscv/run.sh
@@ -8,6 +8,4 @@ cargo build --target riscv32imc-unknown-none-elf
 
 # Start QEMU
 $QEMU -machine virt -nographic -serial mon:stdio --no-reboot \
-    -kernel target/riscv32imc-unknown-none-elf/debug/kernel \
-    -drive id=drive0,file=lorem.txt,format=raw,if=none \
-    -device virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0 \
+    -kernel target/riscv32imc-unknown-none-elf/debug/kernel
