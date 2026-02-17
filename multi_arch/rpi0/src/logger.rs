40fa14fb4b928e391222a89b9c07731e1897582b
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2026-02-17 11:14:50 -0800

    Almost perfect raspi 0-2 hello world.

--- multi_arch/rpi0/src/logger.rs
+++ multi_arch/rpi0/src/logger.rs
@@ -1,6 +1,12 @@
-use log::{LevelFilter, Log, max_level, set_logger, set_max_level};
+use core::fmt::Write;
 
-struct Logger;
+use arm_pl011_uart::Uart;
+use log::{LevelFilter, Log, max_level, set_logger, set_max_level};
+use spin::{Once, mutex::Mutex};
+
+struct Logger {
+    uart: Mutex<Option<Uart<'static>>>,
+}
 
 impl Log for Logger {
     fn enabled(&self, metadata: &log::Metadata) -> bool {
@@ -8,16 +14,24 @@ impl Log for Logger {
     }
 
     fn log(&self, record: &log::Record) {
-        #[cfg(feature = "semihosting")]
-        semihosting::println!("{}", record.args());
+        let mut writer: Option<&mut dyn Write> = if let Some(uart) = self.uart.) {
+            Some()
+        } else {
+            #[cfg(feature = "semihosting")]
+            semihosting::println!("{}", record.args());
+        };
     }
 
     fn flush(&self) {}
 }
 
-static LOGGER: Logger = Logger;
+static LOGGER: Logger = Logger { uart: Once::new() };
 
 pub fn init() {
     set_logger(&LOGGER).unwrap();
     set_max_level(LevelFilter::Trace);
+}
+
+pub fn init_uart(uart: Uart<'static>) {
+    *LOGGER.uart.lock() = Some(uart);
 }
--- multi_arch/rpi0/src/main.rs
+++ multi_arch/rpi0/src/main.rs
@@ -5,12 +5,14 @@
 mod logger;
 
 use core::{
-    arch::{arm::__wfe, asm, naked_asm},
+    arch::{arm::__wfe, naked_asm},
     fmt::Write,
     panic::PanicInfo,
     ptr::NonNull,
 };
 
+use aarch32_cpu::register::Midr;
+use arbitrary_int::u12;
 use arm_pl011_uart::{Uart, UniqueMmioPointer};
 use ez_mailbox::{
     Mailbox, MailboxVolatileFieldAccess, board_revision, call, get_board_revision,
@@ -37,107 +39,126 @@ static BUNDLED_DEVICE_TREES: phf::Map<usize, &[u8]> = phf_map! {
     0xC42 => include_bytes!("../bcm2708-rpi-zero.dtb")
 };
 
-/// Reads the Main ID Register (MIDR) from Coprocessor 15.
-/// Returns a 32-bit value containing the CPU implementer, variant, and part number.
-pub fn read_midr() -> usize {
-    let reg: usize;
-    unsafe {
-        asm!(
-            "mrc p15, 0, {0}, c0, c0, 0",
-            out(reg) reg,
-            options(nomem, nostack, preserves_flags)
-        );
-    }
-    reg
-}
-
 #[unsafe(no_mangle)]
-extern "C" fn kernel_main(_r0: usize, machine_id: usize, atags_ptr: usize) -> ! {
+extern "C" fn kernel_main(_r0: usize, _machine_id: usize, _atags_ptr: usize) -> ! {
     logger::init();
 
-    info!("Hello from Rust kernel. Machine id: {machine_id:#X}. ATAGs ptr: {atags_ptr:#X}.");
-    let midr = read_midr();
-    let part_number = ((midr >> 4) & 0xFFF) as u16;
-    info!("Main ID: {midr:#X}. Part number: {part_number:#X}");
-    let magic = unsafe { (atags_ptr as *const [u32; 2]).read() };
-    info!("ATAGS / Device Tree magic: {magic:#X?}");
+    // info!("Hello from Rust kernel. Machine id: {machine_id:#X}. ATAGs ptr: {atags_ptr:#X}.");
+    info!(
+        "Hello from Rust kernel booted on 32 bit ARM. This is likely booted on a Raspberry Pi Zero, 1, or 2."
+    );
+    let midr = Midr::read();
+    info!("Midr: {midr:?}");
+    // let magic = unsafe { (atags_ptr as *const [u32; 2]).read() };
+    // info!("ATAGS / Device Tree magic: {magic:#X?}");
 
-    if machine_id == 0xC42 {
-        let mbox_ptr = NonNull::new(0x2000B880 as *mut Mailbox).unwrap();
-        let mut mbox = unsafe { VolatileRef::new(mbox_ptr) };
-        let mut buffer = get_board_revision();
-        info!("calling... Mailbox: {:#X}", mbox.as_ptr().status().read());
-        let ok = call(mbox.as_mut_ptr(), 8, &mut buffer);
-        let board_revision = board_revision(&buffer);
-        info!("ok? {ok}. board revision: {board_revision:#X}");
-    }
-
-    // error!("hello QEMU");
-    // error!("hello QEMU");
-    // error!("hello QEMU");
-    // error!("hello QEMU");
-    if let Some(device_tree) = BUNDLED_DEVICE_TREES.get(&machine_id) {
-        let fdt = Fdt::new_unaligned(device_tree).unwrap();
-        let chosen = fdt.root().chosen();
-        if let Some(stdout) = chosen.stdout() {
-            info!("found stdout: {:#?}", stdout);
-            let compatible = stdout.node.property::<Compatible>().unwrap();
-            info!("serial compatible: {compatible:?}");
-            for range in stdout.node.reg().unwrap().iter::<u32, u32>() {
-                info!("reg: {range:X?}");
-            }
-            let mut node = stdout.node;
-            loop {
-                if let Some(parent) = node.parent() {
-                    info!("parent: {parent:?}");
-                    if let Some(ranges) = node.ranges() {
-                        info!("has ranges");
-                        for range in ranges.iter::<usize, usize, usize>() {
-                            info!("range: {range:?}");
-                        }
-                    } else {
-                        info!("does not have ranges");
-                    }
-                    node = parent;
-                } else {
-                    info!("no parent");
-                    break;
-                }
-            }
-
-            if stdout
-                .node
-                .property::<Compatible>()
-                .unwrap()
-                .compatible_with("arm,pl011-axi")
-            {
-                info!("compatible!");
-                // FIXME: Proper address translation
-                let address = NonNull::new(
-                    (stdout
-                        .node
-                        .reg()
-                        .unwrap()
-                        .iter::<u32, u32>()
-                        .next()
-                        .unwrap()
-                        .unwrap()
-                        .address
-                        - 0x7e000000
-                        + 0x20000000) as usize as *mut _,
-                )
-                .unwrap();
-                info!("address: {address:?}");
-
-                let mut uart = Uart::new(unsafe { UniqueMmioPointer::new(address) });
-                uart.write_str("Hello from UART \"driver\"\n").unwrap();
-            }
-        } else {
-            info!("no stdout!");
+    // Figure out what machine we are running on
+    // https://wiki.osdev.org/Detecting_Raspberry_Pi_Board
+    match midr.part_no() {
+        i if i == u12::new(0xB76) => {
+            info!("Running on a Raspberry Pi Zero or 1");
+            // It's safe to use this UART on any computer after this check.
+            const MMIO_BASE: usize = 0x20000000;
+            const GPIO_BASE: usize = MMIO_BASE + 0x200000;
+            const UART0_BASE: usize = GPIO_BASE + 0x1000;
+            let ptr = NonNull::new(UART0_BASE as *mut _).unwrap();
+            let mut uart = Uart::new(unsafe { UniqueMmioPointer::new(ptr) });
+            uart.write_str("Hello from UART driver.\n").unwrap();
         }
-    } else {
-        error!("No device tree for machine id. Not doing anything.");
+        i if i == u12::new(0xC07) => {
+            info!("Running on a Raspberry Pi 2");
+            // It's safe to use this UART on any computer after this check.
+            const MMIO_BASE: usize = 0x3F000000;
+            const GPIO_BASE: usize = MMIO_BASE + 0x200000;
+            const UART0_BASE: usize = GPIO_BASE + 0x1000;
+            let ptr = NonNull::new(UART0_BASE as *mut _).unwrap();
+            let mut uart = Uart::new(unsafe { UniqueMmioPointer::new(ptr) });
+            uart.write_str("Hello from UART driver.\n").unwrap();
+        }
+        // Raspberry pi 3 and 4 also have a part number, but they should not boot 32-bit so we
+        // won't support them in our 32-bit boot process and only support them in our 64-bit
+        // boot process. Also, I'm pretty sure qemu doesn't even allow you to use 64-bit Raspberry
+        // Pis in 32-bit mode.
+        part_no => {
+            info!("Unknown part number: {part_no:#X}. Not doing anything.");
+        }
     }
+    // if machine_id == 0xC42 {
+    //     let mbox_ptr = NonNull::new(0x2000B880 as *mut Mailbox).unwrap();
+    //     let mut mbox = unsafe { VolatileRef::new(mbox_ptr) };
+    //     let mut buffer = get_board_revision();
+    //     info!("calling... Mailbox: {:#X}", mbox.as_ptr().status().read());
+    //     let ok = call(mbox.as_mut_ptr(), 8, &mut buffer);
+    //     let board_revision = board_revision(&buffer);
+    //     info!("ok? {ok}. board revision: {board_revision:#X}");
+    // }
+
+    // error!("hello QEMU");
+    // error!("hello QEMU");
+    // error!("hello QEMU");
+    // error!("hello QEMU");
+    // if let Some(device_tree) = BUNDLED_DEVICE_TREES.get(&machine_id) {
+    //     let fdt = Fdt::new_unaligned(device_tree).unwrap();
+    //     let chosen = fdt.root().chosen();
+    //     if let Some(stdout) = chosen.stdout() {
+    //         info!("found stdout: {:#?}", stdout);
+    //         let compatible = stdout.node.property::<Compatible>().unwrap();
+    //         info!("serial compatible: {compatible:?}");
+    //         for range in stdout.node.reg().unwrap().iter::<u32, u32>() {
+    //             info!("reg: {range:X?}");
+    //         }
+    //         let mut node = stdout.node;
+    //         loop {
+    //             if let Some(parent) = node.parent() {
+    //                 info!("parent: {parent:?}");
+    //                 if let Some(ranges) = node.ranges() {
+    //                     info!("has ranges");
+    //                     for range in ranges.iter::<usize, usize, usize>() {
+    //                         info!("range: {range:?}");
+    //                     }
+    //                 } else {
+    //                     info!("does not have ranges");
+    //                 }
+    //                 node = parent;
+    //             } else {
+    //                 info!("no parent");
+    //                 break;
+    //             }
+    //         }
+
+    //         if stdout
+    //             .node
+    //             .property::<Compatible>()
+    //             .unwrap()
+    //             .compatible_with("arm,pl011-axi")
+    //         {
+    //             info!("compatible!");
+    //             // FIXME: Proper address translation
+    //             let address = NonNull::new(
+    //                 (stdout
+    //                     .node
+    //                     .reg()
+    //                     .unwrap()
+    //                     .iter::<u32, u32>()
+    //                     .next()
+    //                     .unwrap()
+    //                     .unwrap()
+    //                     .address
+    //                     - 0x7e000000
+    //                     + 0x20000000) as usize as *mut _,
+    //             )
+    //             .unwrap();
+    //             info!("address: {address:?}");
+
+    //             let mut uart = Uart::new(unsafe { UniqueMmioPointer::new(address) });
+    //             uart.write_str("Hello from UART \"driver\"\n").unwrap();
+    //         }
+    //     } else {
+    //         info!("no stdout!");
+    //     }
+    // } else {
+    //     error!("No device tree for machine id. Not doing anything.");
+    // }
 
     // let atags_ptr = NonNull::new(atags_ptr as *mut _).unwrap();
     // // Safety: the kernel was given a valid ptr to ATAGS through r2
@@ -148,6 +169,7 @@ extern "C" fn kernel_main(_r0: usize, machine_id: usize, atags_ptr: usize) -> ! 
     }
 }
 
+#[cfg(not(target_feature = "v7"))]
 #[unsafe(link_section = ".text._start")]
 #[unsafe(no_mangle)]
 #[unsafe(naked)]
@@ -180,6 +202,54 @@ extern "C" fn _start() {
             // Else, continue executing the instructions below
             // Call kernel_main
             blx {kernel_main}
+        ",
+        kernel_main = sym kernel_main
+    )
+}
+
+#[cfg(target_feature = "v7")]
+#[unsafe(link_section = ".text._start")]
+#[unsafe(no_mangle)]
+#[unsafe(naked)]
+extern "C" fn _start() {
+    naked_asm!(
+        "
+        // Shut off extra cores
+        mrc p15, 0, r5, c0, c0, 5
+        and r5, r5, #3
+        cmp r5, #0
+	    bne halt
+
+        // Set the stack pointer to the stack space we reserved in the linker script
+        ldr sp, =__stack_top
+
+        // Zero the BSS. Zero it by 4 * usize at a time instead of one byte or one usize at a time
+        ldr r4, =__bss_start
+        ldr r9, =__bss_end
+        // Set r5-r8 to 0
+        mov r5, #0
+        mov r6, #0
+        mov r7, #0
+        mov r8, #0
+        // Start by checking for the end condition
+        b while
+
+        do:
+            // This stores the values of registers r5-r8 at the value of r4, incrementing r4 by
+            // size_of::<usize> as it stores each register
+            stmia r4!, {{r5-r8}}
+
+        while:
+            // If r4 < r9, jump to `do`
+            cmp r4, r9
+            blo do
+            // Else, continue executing the instructions below
+            // Call kernel_main
+            blx {kernel_main}
+
+        halt:
+            wfe
+            b halt
         ",
         kernel_main = sym kernel_main
     )
