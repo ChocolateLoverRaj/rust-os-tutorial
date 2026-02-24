b2b6d10fd9fab6bc2d9eac34ad2ac8647e1204a6
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2026-02-24 08:09:37 -0800

    Organize and document relocation code

--- multi_arch/arm/src/main.rs
+++ multi_arch/arm/src/main.rs
@@ -20,7 +20,6 @@ use ez_mailbox::interrupts::{Interrupts, InterruptsRef};
 use ez_mailbox::timer::{Timer, TimerRef};
 use ez_mailbox::volatile::VolatileRef;
 use log::{error, info};
-use semihosting::println;
 use spin::Once;
 
 use crate::logger::init_uart;
@@ -69,6 +68,7 @@ pub fn panic_handler(panic_info: &PanicInfo) -> ! {
     }
 }
 
+#[cfg(all(target_arch = "arm", not(target_feature = "v7")))]
 #[unsafe(link_section = ".text._header")]
 // Prevent this function from being removed
 #[unsafe(no_mangle)]
@@ -76,84 +76,88 @@ pub fn panic_handler(panic_info: &PanicInfo) -> ! {
 pub extern "C" fn _start() {
     naked_asm!(
         "
-        // mov r3, pc
+        // Get the addres where the start of our kernel was loaded
         sub r3, pc, #8
-        b {start}
+        b {start_common}
         ",
-        start = sym start
-    )
-}
-
-#[cfg(all(target_arch = "arm", not(target_feature = "v7")))]
-#[unsafe(naked)]
-extern "C" fn start() {
-    naked_asm!(
-        "
-        // Set the stack pointer to the stack space we reserved in the linker script
-        ldr sp, =__stack_top
-
-        // Zero the BSS. Zero it by 4 * usize at a time instead of one byte or one usize at a time
-        ldr r4, =__bss_start
-        ldr r9, =__bss_end
-        // Set r5-r8 to 0
-        mov r5, #0
-        mov r6, #0
-        mov r7, #0
-        mov r8, #0
-        // Start by checking for the end condition
-        b while
-
-        do:
-            // This stores the values of registers r5-r8 at the value of r4, incrementing r4 by
-            // size_of::<usize> as it stores each register
-            stmia r4!, {{r5-r8}}
-
-        while:
-            // If r4 < r9, jump to `do`
-            cmp r4, r9
-            blo do
-            // Else, continue executing the instructions below
-            // Call kernel_main
-            blx {kernel_main}
-        ",
-        kernel_main = sym kernel_main_32
+        start_common = sym start_common
     )
 }
 
 #[cfg(all(target_arch = "arm", target_feature = "v7"))]
+#[unsafe(link_section = ".text._header")]
+// Prevent this function from being removed
+#[unsafe(no_mangle)]
 #[unsafe(naked)]
-extern "C" fn start() {
+extern "C" fn _start() {
     naked_asm!(
         "
+        // Get the addres where the start of our kernel was loaded
+        sub r3, pc, #8
+
         // Shut off extra cores
+        // Read the core id
         mrc p15, 0, r5, c0, c0, 5
         and r5, r5, #3
+        // Halt loop if the core id is not 0
         cmp r5, #0
 	    bne halt
 
+		b {start_common}
+
+        halt:
+            wfe
+            b halt
+        ",
+        start_common = sym start_common
+    )
+}
+
+#[unsafe(naked)]
+extern "C" fn start_common() {
+    naked_asm!(
+        "
         // Apply relocations
+        // Get the actual ptr to start of relocations
         ldr r4, =__rel_start
         add r4, r4, r3
+        // Get the actual ptr to end of relocations
         ldr r5, =__rel_end
         add r5, r5, r3
+        // Get the difference in (actual address we're loaded in - the initial address relocations are based on)
         ldr r6, ={kernel_start}
         sub r6, r3, r6
 
+        // Loop through all relocations. Each relocation is a (u32, u32).
+        // The first u32 is an offset from the start of the kernel binary file to the pointer in memory we need to update
+        // The second u32 contains the relocation type in the lower byte
         .reloc_loop:
+            // Exit the loop once we're done
             cmp r4, r5
             beq .reloc_done
 
             // Make sure the relocation is R_ARM_RELATIVE
+            // So far this is the only type of relocation our binary has
+            // But we can handle other types if they show up
+            // Load the second u32
             ldr r7, [r4, #4]
+            // We want to know the type which is stored in the lower 8 bits
             and r7, r7, #0xff
             cmp r7, #0x17
             bne .unknown_reloc
 
+            // Load the first u32
             ldr r7, [r4]
+            // Get the real address of that location in our memory by adjusting by the offset
             add r7, r7, r6
+            // Load the pointer that needs to be adjusted
             ldr r8, [r7]
+            // Adjust the pointer
             add r8, r8, r6
+            // Store the updated value
             str r8, [r7]
+
+            // Go to the next relocation
             add r4, r4, #8
             b .reloc_loop
 
@@ -191,10 +195,6 @@ extern "C" fn start() {
             // Else, continue executing the instructions below
             // Call kernel_main
             blx {kernel_main}
-
-        halt:
-            wfe
-            b halt
         ",
         kernel_main = sym kernel_main,
         kernel_start = const KERNEL_START
@@ -213,7 +213,6 @@ static TIMER_3_COMPLETE: AtomicBool = AtomicBool::new(false);
 #[unsafe(no_mangle)]
 extern "C" fn kernel_main(_r0: usize, _machine_id: usize, _atags_ptr: usize) -> ! {
     logger::init();
-    println!("Hi");
 
     info!(
         "Hello from Rust kernel booted on 32 bit ARM. This is likely booted on a Raspberry Pi Zero, 1, or 2."
