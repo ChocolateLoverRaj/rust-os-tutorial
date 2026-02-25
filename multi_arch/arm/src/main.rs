fb999f6a030e8ae0b80493df76f514cb807cd95b
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2026-02-25 11:58:48 -0800

    Used patched arm-gic crate
    It's a professional crate so it's definitely more preffered

--- multi_arch/arm/src/main.rs
+++ multi_arch/arm/src/main.rs
@@ -6,18 +6,19 @@
 mod logger;
 
 use core::arch::arm::{__wfe, __wfi};
-use core::arch::{asm, naked_asm};
+use core::arch::naked_asm;
 use core::fmt::Write;
 use core::ptr::addr_of;
 use core::sync::atomic::{AtomicBool, Ordering};
 use core::{panic::PanicInfo, ptr::NonNull};
 
-use aarch32_cpu::asm::irq_enable;
+use aarch32_cpu::asm::wfi;
 use aarch32_cpu::interrupt::disable;
 use aarch32_cpu::register::cpsr::ProcessorMode;
 use aarch32_cpu::register::{Cpsr, Midr};
 use arbitrary_int::{u2, u6, u12};
-use arm_gicv2::{GicCpuInterface, GicDistributor};
+use arm_gic::gicv2::GicV2;
+use arm_gic::{IntId, Trigger};
 use arm_pl011_uart::{Uart, UniqueMmioPointer};
 use atags::Atags;
 use collect_array_ext_trait::CollectArray;
@@ -184,7 +185,7 @@ static INTERRUPTS: Once<InterruptsRef> = Once::new();
 static TIMER_1_COMPLETE: AtomicBool = AtomicBool::new(false);
 static TIMER_3_COMPLETE: AtomicBool = AtomicBool::new(false);
 
-static GICC: Once<GicCpuInterface> = Once::new();
+static GIC: Once<Mutex<GicV2>> = Once::new();
 static UART: Once<Mutex<Uart>> = Once::new();
 
 #[unsafe(no_mangle)]
@@ -257,7 +258,7 @@ extern "C" fn kernel_main(
         let fdt = unsafe { Fdt::from_ptr(ptr) };
         match fdt {
             Ok(fdt) => FdtOrAtags::Fdt(fdt),
-            Err(FdtError::InvalidMagic(n)) => FdtOrAtags::Atags({
+            Err(FdtError::InvalidMagic(_)) => FdtOrAtags::Atags({
                 let ptr = NonNull::new(kernel_start as *mut _).unwrap();
                 // Safety: since the pointer is not an FDT is must be an ATAGS
                 unsafe { Atags::new(ptr) }
@@ -277,111 +278,111 @@ extern "C" fn kernel_main(
                             .compatibles()
                             .any(|compatible| compatible == "arm,pl011")
                         {
-                            let mut gic = {
-                                let interrupt_parent = {
-                                    let mut node_path = stdout_path;
-                                    loop {
-                                        let node = fdt
-                                            .find_by_path(match node_path {
-                                                "" => "/",
-                                                node_path => node_path,
-                                            })
-                                            .unwrap();
-                                        if let Some(property) =
-                                            node.find_property("interrupt-parent")
-                                        {
-                                            break Some(property.as_interrupt_parent().unwrap());
-                                        }
-                                        if let Some((parent_path, _)) = node_path.rsplit_once("/") {
-                                            node_path = parent_path;
-                                        } else {
-                                            break None;
-                                        }
-                                    }
-                                };
-                                if let Some(interrupt_parent) = interrupt_parent {
-                                    info!("interrupt parent: {interrupt_parent:#X?}");
-                                    let interrupt_parent = fdt
-                                        .all_nodes()
-                                        .find(|node| {
-                                            if let Some(phandle) = node.find_property("phandle")
-                                                && phandle.as_phandle().unwrap() == interrupt_parent
-                                            {
-                                                true
-                                            } else {
-                                                false
-                                            }
+                            let interrupt_parent = {
+                                let mut node_path = stdout_path;
+                                loop {
+                                    let node = fdt
+                                        .find_by_path(match node_path {
+                                            "" => "/",
+                                            node_path => node_path,
                                         })
                                         .unwrap();
-                                    let interrupt_parent_name = interrupt_parent.name();
-                                    info!("interrupt parent name: {interrupt_parent_name:?}");
-                                    if interrupt_parent
-                                        .compatibles()
-                                        .any(|compatible| compatible == "arm,cortex-a15-gic")
-                                    {
-                                        let [gicd, gicc] = interrupt_parent
-                                            .reg()
-                                            .unwrap()
-                                            .collect_array::<2>()
-                                            .unwrap();
-                                        let mut addresses = [gicd.address, gicc.address];
-                                        fdt.translate_addresses(
-                                            interrupt_parent_name,
-                                            &mut addresses,
-                                        );
-                                        let [gicd, gicc] = addresses;
-                                        let gicd = gicd as *mut _;
-                                        let gicc = gicc as *mut _;
-                                        // Safety: we know the pointers are valid from the device tree
-                                        // let mut gic = unsafe { GicV2::new(gicd, gicc) };
-                                        let mut gicd = GicDistributor::new(gicd);
-                                        let gicc = GicCpuInterface::new(gicc);
-                                        gicd.init();
-                                        gicc.init();
-                                        gicd.configure_interrupt(33, arm_gicv2::TriggerMode::Level);
-                                        gicd.set_enable(33, true);
-                                        GICC.call_once(|| gicc);
-                                        info!("Set up GIC v2");
-                                        if let Some(interrupts_property) =
-                                            stdout.find_property("interrupts")
-                                        {
-                                            let [interrupt_type, relative_id, trigger] =
-                                                interrupts_property
-                                                    .as_u32_iter()
-                                                    .collect_array()
-                                                    .unwrap();
-                                            info!(
-                                                "UART interrupt type: {interrupt_type:#X} , relative_id: {relative_id:#X} , trigger: {trigger:#X}"
-                                            );
-                                            // let int_id = match interrupt_type {
-                                            //     0x0 => IntId::spi(relative_id),
-                                            //     _ => todo!(),
-                                            // };
-                                            // gic.enable_interrupt(int_id, true).unwrap();
-                                            // info!("Enabled UART interrupts in the GIC");
-                                            // let priority_mask = gic.get_priority_mask();
-                                            // info!("Current CPU priority mask: {priority_mask:#X}");
-                                            // gic.set_interrupt_priority(int_id, 0);
-                                            // // unsafe { irq_enable() };
-
-                                            // let sgi_intid = IntId::sgi(3);
-                                            // gic.set_interrupt_priority(sgi_intid, 0x80);
-                                            // gic.enable_interrupt(sgi_intid, true).unwrap();
-                                            // gic.send_sgi(sgi_intid, SgiTarget::All);
-                                            // info!("Sent SGI");
-                                        } else {
-                                            warn!("No interrupts property for UART");
-                                        }
-                                        Some(())
-                                    } else {
-                                        warn!("no driver for interrupt controller");
-                                        None
+                                    if let Some(property) = node.find_property("interrupt-parent") {
+                                        break Some(property.as_interrupt_parent().unwrap());
                                     }
-                                } else {
-                                    warn!("no nterrupts property for UART");
-                                    None
+                                    if let Some((parent_path, _)) = node_path.rsplit_once("/") {
+                                        node_path = parent_path;
+                                    } else {
+                                        break None;
+                                    }
                                 }
                             };
+                            if let Some(interrupt_parent) = interrupt_parent {
+                                info!("interrupt parent: {interrupt_parent:#X?}");
+                                let interrupt_parent = fdt
+                                    .all_nodes()
+                                    .find(|node| {
+                                        if let Some(phandle) = node.find_property("phandle")
+                                            && phandle.as_phandle().unwrap() == interrupt_parent
+                                        {
+                                            true
+                                        } else {
+                                            false
+                                        }
+                                    })
+                                    .unwrap();
+                                let interrupt_parent_name = interrupt_parent.name();
+                                info!("interrupt parent name: {interrupt_parent_name:?}");
+                                if interrupt_parent
+                                    .compatibles()
+                                    .any(|compatible| compatible == "arm,cortex-a15-gic")
+                                {
+                                    let [gicd, gicc] = interrupt_parent
+                                        .reg()
+                                        .unwrap()
+                                        .collect_array::<2>()
+                                        .unwrap();
+                                    let mut addresses = [gicd.address, gicc.address];
+                                    fdt.translate_addresses(interrupt_parent_name, &mut addresses);
+                                    let [gicd, gicc] = addresses;
+                                    let gicd = gicd as *mut _;
+                                    let gicc = gicc as *mut _;
+                                    // Safety: we know the pointers are valid from the device tree
+                                    let mut gic = unsafe { GicV2::new(gicd, gicc) };
+                                    gic.setup();
+                                    // let mut gicd = GicDistributor::new(gicd);
+                                    // let gicc = GicCpuInterface::new(gicc);
+                                    // gicd.init();
+                                    // gicc.init();
+                                    // gicd.configure_interrupt(33, arm_gicv2::TriggerMode::Level);
+                                    // gicd.set_enable(33, true);
+                                    // GICC.call_once(|| gicc);
+                                    info!("Set up GIC v2");
+                                    if let Some(interrupts_property) =
+                                        stdout.find_property("interrupts")
+                                    {
+                                        let [interrupt_type, relative_id, trigger] =
+                                            interrupts_property
+                                                .as_u32_iter()
+                                                .collect_array()
+                                                .unwrap();
+                                        info!(
+                                            "UART interrupt type: {interrupt_type:#X} , relative_id: {relative_id:#X} , trigger: {trigger:#X}"
+                                        );
+                                        let int_id = match interrupt_type {
+                                            0x0 => IntId::spi(relative_id),
+                                            _ => todo!(),
+                                        };
+                                        gic.set_trigger(
+                                            int_id,
+                                            match trigger {
+                                                0x4 => Trigger::Level,
+                                                _ => todo!(),
+                                            },
+                                        );
+                                        gic.set_interrupt_priority(int_id, 0x0);
+                                        gic.enable_interrupt(int_id, true).unwrap();
+                                        info!("Enabled UART interrupts in the GIC");
+                                        // let priority_mask = gic.get_priority_mask();
+                                        // info!("Current CPU priority mask: {priority_mask:#X}");
+                                        // // unsafe { irq_enable() };
+
+                                        // let sgi_intid = IntId::sgi(3);
+                                        // gic.set_interrupt_priority(sgi_intid, 0x80);
+                                        // gic.enable_interrupt(sgi_intid, true).unwrap();
+                                        // gic.send_sgi(sgi_intid, SgiTarget::All);
+                                        // info!("Sent SGI");
+                                    } else {
+                                        warn!("No interrupts property for UART");
+                                    }
+                                    GIC.call_once(|| Mutex::new(gic));
+                                } else {
+                                    warn!("no driver for interrupt controller");
+                                }
+                            } else {
+                                warn!("no nterrupts property for UART");
+                            }
+
                             let fdt_address = stdout.reg().unwrap().next().unwrap().address;
                             let physical_address =
                                 usize::try_from(fdt.translate_address(stdout_path, fdt_address))
@@ -403,7 +404,9 @@ extern "C" fn kernel_main(
                             unsafe { aarch32_cpu::interrupt::enable() };
                             // let sgi_intid = IntId::sgi(3);
                             // gic.as_mut().unwrap().send_sgi(sgi_intid, SgiTarget::All);
-                            loop {}
+                            loop {
+                                wfi();
+                            }
                         } else {
                             warn!("no compatible UART found.");
                             for compatible in stdout.compatibles() {
@@ -528,36 +531,35 @@ extern "C" fn vectors() {
 }
 
 unsafe extern "C" fn exception_handler() {
-    info!("exception handler!");
     let cpsr = Cpsr::read();
     if cpsr.mode() == Ok(ProcessorMode::Irq) {
-        let gicc = GICC.get().unwrap();
-        gicc.handle_irq(|vector| {
-            info!("interrupt vector {vector}");
-            let mut uart = UART.get().unwrap().try_lock().unwrap();
-            let interrupts = uart.raw_interrupt_status();
-            info!("uart interrupts: {interrupts:#X?}");
-            if interrupts.contains(arm_pl011_uart::Interrupts::TXI) {
-                uart.clear_interrupts(arm_pl011_uart::Interrupts::TXI);
-            }
-            loop {
-                match uart.read_word() {
-                    Ok(byte) => {
-                        if let Some(byte) = byte {
-                            let char = char::from_u32(byte.into());
-                            info!("received byte from UART: {byte:#X} ({char:?})");
-                        } else {
-                            break;
-                        }
-                    }
-                    Err(e) => {
-                        error!("uart error: {e}");
+        let mut gic = GIC.get().unwrap().try_lock().unwrap();
+        let int_id = gic.get_and_acknowledge_interrupt().unwrap();
+        info!("interrupt: {int_id:#X?}");
+        let mut uart = UART.get().unwrap().try_lock().unwrap();
+        let interrupts = uart.raw_interrupt_status();
+        info!("uart interrupts: {interrupts:#X?}");
+        if interrupts.contains(arm_pl011_uart::Interrupts::TXI) {
+            uart.clear_interrupts(arm_pl011_uart::Interrupts::TXI);
+        }
+
+        loop {
+            match uart.read_word() {
+                Ok(byte) => {
+                    if let Some(byte) = byte {
+                        let char = char::from_u32(byte.into());
+                        info!("received byte from UART: {byte:#X} ({char:?})");
+                    } else {
                         break;
                     }
                 }
+                Err(e) => {
+                    error!("uart error: {e}");
+                    break;
+                }
             }
-            // uart.clear_interrupts(arm_pl011_uart::Interrupts::all());
-        });
+        }
+        gic.end_interrupt(int_id);
         // let interrupts = INTERRUPTS.get().unwrap();
         // let pending_irqs = interrupts.pending_interrupts_irq_0_32();
         // // info!("pending irqs: {pending_irqs:#X}");
